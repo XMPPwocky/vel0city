df6812149d8f25915b6972c9abd74dc5ae206539
Nathaniel Theis <nttheis@gmail.com>
2015-03-02 13:28:24 -0800

    formatting

--- src/assets.rs
+++ src/assets.rs
@@ -10,7 +10,6 @@ fn name_to_path(name: &str) -> PathBuf {
 }
 
 pub fn load_bin_asset(name: &str) -> io::Result<Vec<u8>> {
-
     let path = name_to_path(name);
 
     let mut v = Vec::new();
@@ -19,8 +18,8 @@ pub fn load_bin_asset(name: &str) -> io::Result<Vec<u8>> {
 }
 
 pub fn load_str_asset(name: &str) -> io::Result<String> {
-
     let path = name_to_path(name);
+
     let mut v = String::new();
     try!(File::open(&path).and_then(|mut f| f.read_to_string(&mut v)));
     Ok(v)
