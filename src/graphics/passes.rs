0d7d26bfa31e216e293409b8966d45e445beceae
Nathaniel Theis <nttheis@gmail.com>
2015-03-28 17:00:09 -0700

    passes

---
+++ src/graphics/passes.rs
@@ -0,0 +1,172 @@
+use graphics::QuadVertex;
+use graphics::Light;
+use glium;
+use glium::Surface;
+
+pub struct Technique {
+    pub shader: glium::Program,
+    pub drawparams: glium::DrawParameters,
+}
+impl Technique {
+    pub fn new(shader: glium::Program) -> Technique {
+        Technique { shader: shader, drawparams: ::std::default::Default::default() }
+    }
+}
+
+pub struct PassData {
+    pub color: glium::Texture2d, 
+    pub light: glium::Texture2d, 
+    pub normal: glium::Texture2d,
+    pub position: glium::Texture2d,
+    pub depth: glium::texture::DepthTexture2d,
+}
+
+impl PassData {
+    pub fn new(d: &glium::Display, dimensions: (u32, u32)) -> PassData {
+        let color = glium::texture::Texture2d::new_empty(&d, glium::texture::UncompressedFloatFormat::F32F32F32F32, dimensions.0, dimensions.1); 
+        let light = glium::texture::Texture2d::new_empty(&d, glium::texture::UncompressedFloatFormat::F32F32F32F32, dimensions.0, dimensions.1); 
+        let normal = glium::texture::Texture2d::new_empty(&d, glium::texture::UncompressedFloatFormat::F32F32F32F32, dimensions.0, dimensions.1); 
+        let position = glium::texture::Texture2d::new_empty(&d, glium::texture::UncompressedFloatFormat::F32F32F32F32, dimensions.0, dimensions.1); 
+        let depth = glium::texture::DepthTexture2d::new_empty(&d, glium::texture::DepthFormat::F32, dimensions.0, dimensions.1); 
+
+        PassData {
+            color: color,
+            light: light,
+            normal: normal,
+            position: position,
+            depth: depth
+        }
+    }
+    pub fn get_framebuffer(&self, display: &glium::Display) -> glium::framebuffer::MultiOutputFrameBuffer {
+        let fboutputs = [
+            ("color_out", &self.color),
+            ("light_out", &self.light),
+            ("normal_out", &self.normal),
+            ("position_out", &self.position),
+        ];
+        glium::framebuffer::MultiOutputFrameBuffer::with_depth_buffer(&display, &fboutputs, &self.depth)
+    }
+}
+
+pub struct PostprocessPassData {
+    pub color: glium::Texture2d, 
+}
+impl PostprocessPassData {
+    pub fn new(d: &glium::Display, dimensions: (u32, u32)) -> PostprocessPassData {
+        let color = glium::texture::Texture2d::new_empty(&d, glium::texture::UncompressedFloatFormat::F32F32F32F32, dimensions.0, dimensions.1); 
+
+        PostprocessPassData {
+            color: color,
+        }
+    }
+    pub fn get_framebuffer(&self, display: &glium::Display) -> glium::framebuffer::SimpleFrameBuffer {
+        glium::framebuffer::SimpleFrameBuffer::new(&display, &self.color)
+    }
+}
+pub struct LightPassData {
+    pub light: glium::Texture2d, 
+}
+impl LightPassData {
+    pub fn new(d: &glium::Display, dimensions: (u32, u32)) -> LightPassData {
+        let light = glium::texture::Texture2d::new_empty(&d, glium::texture::UncompressedFloatFormat::F32F32F32F32, dimensions.0, dimensions.1); 
+
+        LightPassData {
+            light: light,
+        }
+    }
+    pub fn get_framebuffer(&self, display: &glium::Display) -> glium::framebuffer::SimpleFrameBuffer {
+        glium::framebuffer::SimpleFrameBuffer::new(&display, &self.light)
+    }
+}
+pub struct PassSystem {
+    quad_verts: glium::VertexBuffer<QuadVertex>,
+    quad_indices: glium::IndexBuffer,
+}
+impl PassSystem {
+    pub fn new(d: &glium::Display) -> PassSystem {
+        let verts = vec![
+            QuadVertex { position: [-1.0, -1.0] },
+            QuadVertex { position: [1.0, -1.0] },
+            QuadVertex { position: [-1.0, 1.0] },
+            QuadVertex { position: [1.0, 1.0] },
+        ];
+        let indices = glium::index::TriangleStrip(vec![0u8, 1, 2, 3]);
+
+        PassSystem {
+            quad_verts: glium::VertexBuffer::new(d, verts),
+            quad_indices: glium::IndexBuffer::new(d, indices),
+        }
+    }
+
+    pub fn postprocess<S>(&self,
+                       display: &glium::Display,
+                       input_prepass: &PassData, 
+                       input_light: &LightPassData, 
+                       output: &mut S, 
+                       technique: &Technique) where S: glium::Surface { 
+        let colorsamp = glium::uniforms::Sampler::new(&input_prepass.color)
+            .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+        let normsamp = glium::uniforms::Sampler::new(&input_prepass.normal)
+            .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+        let possamp = glium::uniforms::Sampler::new(&input_prepass.position)
+            .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+        let depthsamp = glium::uniforms::Sampler::new(&input_prepass.depth)
+            .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+        let lightsamp = glium::uniforms::Sampler::new(&input_light.light)
+            .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+        let uniforms = uniform! {
+            color_texture: colorsamp,
+            normal_texture: normsamp,
+            position_texture: possamp, 
+            depth_texture: depthsamp,
+            light_texture: lightsamp
+        };
+
+        output.draw(&self.quad_verts,
+                         &self.quad_indices,
+                         &technique.shader,
+                         &uniforms,
+                         &technique.drawparams).unwrap();
+    }
+
+    pub fn light_passes(&self,
+                        display: &glium::Display,
+                       input: &PassData, 
+                       output: &mut LightPassData, 
+                       lights: &[Light],
+                       technique: &Technique) { 
+        let mut framebuffer = output.get_framebuffer(display);
+        for light in lights {
+            let colorsamp = glium::uniforms::Sampler::new(&input.color)
+                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+            let normsamp = glium::uniforms::Sampler::new(&input.normal)
+                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+            let possamp = glium::uniforms::Sampler::new(&input.position)
+                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+            let depthsamp = glium::uniforms::Sampler::new(&input.depth)
+                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+            let lightsamp = glium::uniforms::Sampler::new(&input.light)
+                .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear);
+
+            let uniforms = uniform! {
+                color_texture: colorsamp,
+                normal_texture: normsamp,
+                position_texture: possamp, 
+                depth_texture: depthsamp,
+                light_texture: lightsamp,
+
+                light_position: *light.position.as_array(),
+                light_intensity: light.intensity,
+                light_attenuation: light.attenuation,
+                light_color: *light.color.as_array()
+            };
+
+            framebuffer.draw(&self.quad_verts,
+                        &self.quad_indices,
+                        &technique.shader,
+                        &uniforms,
+                        &technique.drawparams).unwrap();
+        }
+    }
+}
+
---
+++ src/particle.rs
@@ -0,0 +1,35 @@
+use na;
+pub struct Particle {
+    pub position: na::Vec3<f32>,
+    pub spawntime: f32
+}
+
+pub struct ParticleSystem {
+    pub particles: Vec<Particle>,
+    pub particles_count: u32,
+
+    pub lifetime: f32,
+}
+impl ParticleSystem {
+    pub fn update(&mut self, curtime: f32, _dt: f32) {
+        let dead = curtime - self.lifetime;
+
+        let particles_capacity = self.particles.len();
+        for (idx, particle) in self.particles.iter_mut().rev().take(self.particles_count as usize).enumerate() {
+            if particle.spawntime < dead {
+                self.particles_count = idx as u32;
+                break;
+            }
+        }
+    }
+    pub fn add(&mut self, particle: Particle) {
+        self.particles_count += 1;
+        let particles_capacity = self.particles.len();
+        if self.particles_count as usize > particles_capacity {
+            self.particles.push(particle);
+        } else {
+            self.particles[particles_capacity - self.particles_count as usize] = particle;
+        }
+    }
+
+}
